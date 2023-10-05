870776fad79943d516143db056231c53bc100bb5
Harpalak <121934878+Harpalak@users.noreply.github.com>
2023-10-05 10:57:58 +0530

    Initial Commit

---
+++ AzurePOC/AzurePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzurePOCApp.swift
+//  AzurePOC
+//
+//  Created by Harpalak Kaur Kang on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AzurePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
